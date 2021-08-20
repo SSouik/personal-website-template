e7ed129c286e6db040c51aa0b8879b032bb41a59
ssouik <sam.souik@gmail.com>
2021-08-19 22:45:09 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <div className='banner'>
+                <div className='row p-5'>
+                    <div className='col-md-12 text-center'>
+                        Using bootstrap now
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
